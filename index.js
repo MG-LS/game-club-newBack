fc14779512299ed63fc8374d4255cf869605c6f9
Baysarov <adambajsarov@gmail.com>
2022-04-19 12:45:00 +0300

    adam

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 require("dotenv").config();
 //жижжг галнаш дез
 app.use(cors());
 app.use(express.json());
+app.use("/image", express.static(path.resolve(__dirname, "image")));
 app.use(require("./routes/users.route"));
 app.use(require("./routes/news.route"));
 app.use(require("./routes/comments.route"));
